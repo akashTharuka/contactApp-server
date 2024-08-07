53c74e577b7444644c0fdf9881fb25b771a8c8b0
akashTharuka <akash_tharuka@yahoo.com>
2024-08-07 10:43:57 +0530

    added ContactRepo interface

--- src/main/java/com/personal/contactlist/repo/ContactRepo.java
+++ src/main/java/com/personal/contactlist/repo/ContactRepo.java
@@ -1,2 +1,13 @@
-package com.personal.contactlist.repo;public interface ContactRepo {
+package com.personal.contactlist.repo;
+
+import com.personal.contactlist.domain.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContactRepo extends JpaRepository<Contact, String> {
+    @Override
+    Optional<Contact> findById(String id);
 }
